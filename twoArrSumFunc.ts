575b95733de75db20128fbdff1b4441e81c05312
Rosana Yumi <rosanayumiyamamura1@gmail.com>
2023-04-22 15:10:15 -0300

    new

--- twoArrSumFunc.ts
+++ twoArrSumFunc.ts
@@ -8,31 +8,17 @@ El arreglo tiene dimensión 6 y los números de los dos vectores los carga el us
 
 let arr1: number[] = new Array(6);
 let arr2: number[] = new Array(6);
-
-
+let arr3: number[] = new Array(6);
 
 
 function cargarArrNum(arr: number[]): void {
-  if (!arr.length) return;
-  console.log('Inicia carga de arreglo');
   for (let i: number = 0; i < arr.length; i++) {
     arr[i] = r.questionInt(`Input number in position ${i + 1}: `);
   }
-  console.log('Arreglo cargado');
-  console.log(arr);
 }
 
-
-function twoArrSum(arrA: number[], arrB: number[]): number[] {
-  if (arrA.length !== arrB.length) return [];
-  let resultArr: number[] = new Array(arrA.length);
-  for (let i: number = 0; i < arrA.length; i++) {
-    resultArr[i] = arrA[i] + arrB[i];
-  }
-  return resultArr;
-}
-
-const result: number[] = twoArrSum(arr1, arr2);
-console.log(result);
+cargarArrNum(arr1);
+cargarArrNum(arr2);
+console.log(arr1,arr2);
 
 
